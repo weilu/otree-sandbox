5684f843d9392469e2b327b848130a52f521653f
Wei Lu <luwei.here@gmail.com>
2020-06-06 09:36:33 -0400

    minimal sample project to reproduce alert box styling issue

---
+++ survey/pages.py
@@ -0,0 +1,15 @@
+from otree.api import Currency as c, currency_range
+from ._builtin import Page, WaitPage
+from .models import Constants
+
+
+class MyPage(Page):
+    form_fields = ['name']
+    form_model = 'player'
+
+
+class Results(Page):
+    pass
+
+
+page_sequence = [MyPage, Results]
